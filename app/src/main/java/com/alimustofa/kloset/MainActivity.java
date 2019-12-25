82c50c587b67f6d9a08c3534314732e3b54bd32b
Alimustoofaa <56061857+Alimustoofaa@users.noreply.github.com>
2019-12-25 23:36:48 +0700

    test

--- app/src/main/java/com/alimustofa/kloset/MainActivity.java
+++ app/src/main/java/com/alimustofa/kloset/MainActivity.java
@@ -1,5 +1,6 @@
 package com.alimustofa.kloset;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         //init views
         mBtnRegister = findViewById(R.id.btn_register);

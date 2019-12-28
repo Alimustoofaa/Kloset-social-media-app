c73a19472b9338631b7dcc7317935bda0e689c39
Alimustoofaa <56061857+Alimustoofaa@users.noreply.github.com>
2019-12-28 08:21:41 +0700

    handle dont't have account

--- app/src/main/java/com/alimustofa/kloset/LoginActivity.java
+++ app/src/main/java/com/alimustofa/kloset/LoginActivity.java
@@ -3,9 +3,14 @@ package com.alimustofa.kloset;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView mdontthave_accountTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +24,26 @@ public class LoginActivity extends AppCompatActivity {
         //enable button
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
+
+        mdontthave_accountTV = findViewById(R.id.dontthave_accountTV);
+
+        //handle donthave account
+        mdontthave_accountTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
     }
+
+
 
     @Override
     public boolean onSupportNavigateUp(){
         onBackPressed();//go previous activity
         return super.onSupportNavigateUp();
     }
+
+
 }

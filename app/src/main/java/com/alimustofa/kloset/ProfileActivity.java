5b1fcc7f42416b7df699b9674f1280189a50e5ba
Alimustoofaa <56061857+Alimustoofaa@users.noreply.github.com>
2020-01-03 21:53:01 +0700

    menu logout

--- app/src/main/java/com/alimustofa/kloset/ProfileActivity.java
+++ app/src/main/java/com/alimustofa/kloset/ProfileActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -14,6 +17,8 @@ public class ProfileActivity extends AppCompatActivity {
     //Firebase auth
     FirebaseAuth firebaseAuth;
 
+    //view
+    TextView mProfileTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +30,9 @@ public class ProfileActivity extends AppCompatActivity {
 
         //init
         firebaseAuth = FirebaseAuth.getInstance();
+
+        //init views
+        mProfileTV = findViewById(R.id.profileTV);
     }
 
     private void checkUserStatus(){
@@ -32,6 +40,9 @@ public class ProfileActivity extends AppCompatActivity {
         FirebaseUser user = firebaseAuth.getCurrentUser();
         if(user !=null){
             //user sign and stay here
+
+            //set email of loggged in user
+            mProfileTV.setText("Hallo "+user.getEmail());
         }else{
             startActivity(new Intent(ProfileActivity.this, MainActivity.class));
             finish();
@@ -42,5 +53,29 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onStart() {
         checkUserStatus();
         super.onStart();
+    }
+
+    //inflate action menu
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        //indlating menu
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //handle menu item
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //get item id
+        int id = item.getItemId();
+        if(id == R.id.action_logout){
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

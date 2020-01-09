bb7b588ab7c590413657849d8ca90512e4b6f4a8
Alimustoofaa <56061857+Alimustoofaa@users.noreply.github.com>
2020-01-10 00:41:45 +0700

    show users

---
+++ app/src/main/java/com/alimustofa/kloset/AdapterUser.java
@@ -0,0 +1,86 @@
+package com.alimustofa.kloset;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterUser extends RecyclerView.Adapter<AdapterUser.MyHolder> {
+
+    Context context;
+    List<ModelUsers>usersList;
+
+    //constructor
+    public AdapterUser(Context context, List<ModelUsers> usersList) {
+        this.context = context;
+        this.usersList = usersList;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        //inflate layout (row_user.xml)
+        View view= LayoutInflater.from(context).inflate(R.layout.row_users, viewGroup, false);
+        return new  MyHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int i) {
+        //get data
+        String userImage = usersList.get(i).getImage();
+        String userName = usersList.get(i).getName();
+        final String userEmail = usersList.get(i).getEmail();
+
+        //set data
+        holder.mNameTv.setText(userName);
+        holder.mEmailTv.setText(userEmail);
+        try{
+            Picasso.get().load(userImage).placeholder(R.drawable.ic_dafault_img).into(holder.mavatarIv);
+
+        }catch (Exception e){
+
+        }
+
+        //handle item click
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, ""+userEmail, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return usersList.size();
+    }
+
+    //view holde class
+    class MyHolder extends RecyclerView.ViewHolder{
+
+        ImageView mavatarIv;
+        TextView mNameTv, mEmailTv;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //init view
+            mavatarIv=itemView.findViewById(R.id.avatarIv);
+            mNameTv = itemView.findViewById(R.id.nameTv);
+            mEmailTv = itemView.findViewById(R.id.emailTv);
+
+        }
+    }
+}

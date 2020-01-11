eb6440053052d517fc5e90eeebcf285896fda1b8
Alimustoofaa <56061857+Alimustoofaa@users.noreply.github.com>
2020-01-11 21:43:44 +0700

    add menu logout in Chat Activity

--- app/src/main/java/com/alimustofa/kloset/ChatActivity.java
+++ app/src/main/java/com/alimustofa/kloset/ChatActivity.java
@@ -6,12 +6,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class ChatActivity extends AppCompatActivity {
@@ -23,6 +29,8 @@ public class ChatActivity extends AppCompatActivity {
     TextView nameTv, userStatusTv;
     EditText massageEt;
     ImageButton sendBtn;
+
+    FirebaseAuth firebaseAuth;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -42,5 +50,41 @@ public class ChatActivity extends AppCompatActivity {
         userStatusTv = findViewById(R.id.userStatusTv);
         massageEt = findViewById(R.id.massageEt);
         sendBtn = findViewById(R.id.sendBtn);
+
+        //firebases auth instance
+        firebaseAuth = FirebaseAuth.getInstance();
+
+    }
+    private void checkUserStatus(){
+        //get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user !=null){
+            //user sign and stay here
+
+            //set email of loggged in user
+
+        }else{
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        //hiden search view as we dont need it here
+        menu.findItem(R.id.action_serach).setVisible(false);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.action_logout){
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 }

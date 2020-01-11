4659bdad5ddd97d37acb7197843b0817fed13f6d
Alimustoofaa <56061857+Alimustoofaa@users.noreply.github.com>
2020-01-11 23:00:38 +0700

    send message

--- app/src/main/java/com/alimustofa/kloset/ChatActivity.java
+++ app/src/main/java/com/alimustofa/kloset/ChatActivity.java
@@ -1,5 +1,6 @@
 package com.alimustofa.kloset;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,16 +9,28 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 
 
 public class ChatActivity extends AppCompatActivity {
@@ -31,6 +44,12 @@ public class ChatActivity extends AppCompatActivity {
     ImageButton sendBtn;
 
     FirebaseAuth firebaseAuth;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databasesUser;
+
+    String hisUid;
+    String myUid;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -51,10 +70,78 @@ public class ChatActivity extends AppCompatActivity {
         massageEt = findViewById(R.id.massageEt);
         sendBtn = findViewById(R.id.sendBtn);
 
+
+        Intent intent= getIntent();
+        hisUid = intent.getStringExtra("hisUID");
+
         //firebases auth instance
         firebaseAuth = FirebaseAuth.getInstance();
 
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databasesUser = firebaseDatabase.getReference("Users");
+
+        //search user to get that user info
+        Query userQuery = databasesUser.orderByChild("uid").equalTo(hisUid);
+
+        //get user picture and name
+        userQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //check until required info is receive
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    //get data
+                    String name = ""+ds.child("name").getValue();
+                    String  image =""+ ds.child("image").getValue();
+
+                    //set data
+                    nameTv.setText(name);
+                    try{
+                        Picasso.get().load(image).into(profileIv);
+                    }catch (Exception e){
+                        Picasso.get().load(R.drawable.ic_dafault_img_white).into(profileIv);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get text from edit text
+                String message = massageEt.getText().toString().trim();
+
+                //check if text is empty or not
+                if (TextUtils.isEmpty(message)){
+                    //text empty
+                    Toast.makeText(ChatActivity.this, "Cannot send the empty message..", Toast.LENGTH_SHORT).show();
+                }else {
+                    //text not empty
+                    sendMessage(message);
+
+                }
+            }
+        });
+
     }
+
+    private void sendMessage(String message) {
+
+        DatabaseReference databaseReference =FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", myUid);
+        hashMap.put("receiver", hisUid);
+        hashMap.put("message", message);
+        databaseReference.child("Chats").push().setValue(hashMap);
+
+        //reset edit text after sending message
+        massageEt.setText("");
+    }
+
     private void checkUserStatus(){
         //get current user
         FirebaseUser user = firebaseAuth.getCurrentUser();
@@ -62,11 +149,17 @@ public class ChatActivity extends AppCompatActivity {
             //user sign and stay here
 
             //set email of loggged in user
-
+            myUid =user.getUid();//currently dignrf in user uif
         }else{
             startActivity(new Intent(this, MainActivity.class));
             finish();
         }
+    }
+
+    @Override
+    protected void onStart() {
+        checkUserStatus();
+        super.onStart();
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
